893cbed49bffa3c8260c0f67480bf2b124c3e5a3
Sonam Ghosh <sonamghosh@gmail.com>
2018-12-22 01:15:36 -0500

    Changed structure of code to Object Orientated style , still needs to be tested, functionality for recent forms added but not past forms

--- Edgar13F-Parser/edgar13f_parser.py
+++ Edgar13F-Parser/edgar13f_parser.py
@@ -1,174 +1,96 @@
-import numpy as np
-import pandas as pd
-import os
+# Modifying existing code above
+
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup, SoupStrainer
 import requests
 import csv
-import datetime
-import sys
-from bs4 import BeautifulSoup
+import pandas as pd
 import errno
-from itertools import repeat
-from requests_html import HTMLSession
-import re
-import xmltodict
-import xml.etree.ElementTree as et
+import sys
+import os
 
 
-def parse_doc(ticker=None, cik=None, prior_to=None, count=100):
-    # Find CIK if given only ticker since needed for query
-    if ticker != None and cik == None:
-        valid = ticker_validity_checker(ticker)  # Checks if ticker is valid, raises error if not valid
-        cik = get_cik(ticker)
-    # Creates diredtory to store information in
-    try:
-        make_dir(cik, prior_to)
-    except Exception as e:
-        print(str(e))
+class EdgarParser:
+    base_url = 'https://www.sec.gov'
+    latest_form_query = '/cgi-bin/browse-edgar?action=getcompany&CIK={}'
 
-    # generate url
-    edgar_url = 'http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK='+str(cik)+'&type=13F&dateb='+\
-                str(prior_to)+'&owner=exclude&output=xml&count='+str(count)
+    def __init__(self, cik=None, ticker=None):
+        if cik:
+            self.cik = cik
+        # Find CIK if given only the ticker and validate it
+        else:
+            self.ticker = ticker
+            self.valid = ticker_validity_checker(self.ticker)
+            if not self.valid:
+                raise ValueError('This is not a valid ticker: ', self.ticker)
+            self.cik = get_cik(self.ticker)
 
-    print('Parsing 13-F' + str(ticker))
-    r = requests.get(edgar_url)
-    data = r.text
+        # Required query links
+        self.base_url = 'https://www.sec.gov'
+        self.latest_form_url = '/cgi-bin/browse-edgar?action=getcompany&CIK='+str(self.cik)+'&type=13F'
+        #self.prev_form_url = self.latest_form_url + '&dateb='
 
-    print(data)
-    doc_list, doc_name_list = create_document_list(data)
+        # Start Session
+        self.session = requests.Session()
 
-    print(doc_list)
-    print(doc_name_list)
+    def ticker_validity_checker(self, ticker):
+        # Open CSV file with mutual fund tickers into pandas dataframe
+        data = pd.read_csv('mutual_fund_ticker_list.csv')
+        # Check efficiently if ticker exists in the csv file
+        valid = ticker in set(data.Ticker)
+        if not valid:
+            return False
+        else:
+            return True
 
-    try:
-        save_to_dir(cik, prior_to, doc_list, doc_name_list)
-        #save_to_tsv(cik, doc_list, doc_name_list)
-    except Exception as e:
-        print(str(e))
+    def get_cik(self, ticker):
+        # Start HTML Session
+        url = 'https://www.sec.gov/cgi-bin/series?ticker=' + \
+               ticker+'&CIK=&sc=companyseries&type=N-PX&Find=Search'
+        r = session.get(url)
+        links = list(r.html.absolute_links)
+        ticker_link = links[-3]  # 2nd last one contains CIK
+        # Extract the CIK
+        search_string = re.search('CIK=(.+?)&action', ticker_link)
+        if search_string:
+            found = search_string.group(1)
 
-    print('Success')
+        return found
 
-def create_document_list(data):
-    # parse data with beautiful soup
-    soup = BeautifulSoup(data, features='lxml')
-    # Store link in list
-    link_list = list()
+    def parse_recent_doc(self, cik):
+        # Parses information from the most recent 13 F form
 
-    # Convert .htm's to .html 
-    for link in soup.find_all('filinghref'):
-        url = link.string
-        if link.string.split('.')[len(link.string.split('.'))-1] == 'htm':
-            url += 'l'
-        link_list.append(url)
+        # Query link
+        url = urljoin(self.base_url, self.latest_form_url)
 
-    link_list_tot = link_list
+        # grab the most recent form
+        parse = SoupStrainer('a', {'id': 'documentsbutton'})
+        soup = BeautifulSoup(self.session.get(url).content, 'lxml',
+                             parse_only=parse_only)
+        recent_form_url = soup.find('a', {'id': 'documentsbutton'})['href']
+        recent_form_url = urljoin(self.base_url, recent_form_url)
 
-    print('Number of files downloading {0}'.format(len(link_list_tot)))
+        # grab document url for the form
+        parse = SoupStrainer('tr', {'class': 'blueRow'})
+        soup = BeautifulSoup(self.session.get(recent_form_url).content, 'lxml',
+                             parse_only=parse_only)
+        form_url = soup.find_all('tr', {'class': 'blueRow'})[-1].find('a')['href']
+        form_url = urljoin(self.base_url, form_url)
 
-    # List of url to the text documents
-    doc_list = list()
-    # List of doc names
-    doc_name_list = list()
+        return self.extract_information(form_url)
 
-    # Get all the doc
-    for i in range(len(link_list_tot)):
-        required_url = link_list_tot[i].replace('-index.html', '')
-        txtdoc = required_url + '.txt'
-        docname = txtdoc.split("/")[-1]
-        doc_list.append(txtdoc)
-        doc_name_list.append(docname)
+    def extract_information(self, url):
+        # Extract Holdings information from 13-F document
+        soup = BeautifulSoup(self.session.get(url).content, 'lxml')
 
-    return doc_list, doc_name_list
+        holdings = {h.text for h in soup.find_all((lambda tag: 'issuer' in tag.name.lower()))}
+        if not holdings:
+            print('No holdings at: {}'.format(url))
+            return
 
-
-def get_cik(ticker):
-    session = HTMLSession()
-    url = 'https://www.sec.gov/cgi-bin/series?ticker='+ticker+'&CIK=&sc=companyseries&type=N-PX&Find=Search'
-    r = session.get(url)
-    links = list(r.html.absolute_links)
-    ticker_link = links[-3]  # Last two links in the Set are irrelevant, 2nd last one contains one corresponding to ticker
-    # Extract the CIK
-    search_string = re.search('CIK=(.+?)&action', ticker_link)
-    if search_string:
-        found = search_string.group(1)
-
-    return found
-
-
-def ticker_validity_checker(ticker):
-    # Open CSV file with mutual fund tickers into pandas dataframe
-    data = pd.read_csv('mutual_fund_ticker_list.csv')
-    # Check efficiently by seeing if the ticker exists in the ticker column parsed into a Set object
-    valid = ticker in set(data.Ticker)
-    if not valid:
-        raise ValueError('This is not a valid ticker: ', ticker)
-    else:
-        return True
+        return Holdings
 
 
-def make_dir(cik, prior_to):
-    # Get current directory
-    current_dir = os.getcwd()
-    final_dir = os.path.join(current_dir, r'SEC-Edgar-Data')
-
-    # Make dir to save company filings
-    path = os.path.join(final_dir, cik)
-    print('Path ==== ', path)
-    if not os.path.exists(path):
-        try:
-            os.makedirs(path)
-        except OSError as exception:
-            if exception.errno != errno.EEXIST:
-                raise
-    
-
-
-def save_to_dir(cik, prior_to, doc_list, doc_name_list):
-    default_data_path = os.path.join(os.getcwd(), 'SEC-Edgar-Data')
-    # Save every text document into its respective folder
-    for i in range(len(doc_list)):
-        base_url = doc_list[i]
-        r = requests.get(base_url)
-        data = r.text
-        path = os.path.join(default_data_path, cik, doc_name_list[i])
-
-        with open(path, 'ab') as f:
-            f.write(data.encode('ascii', 'ignore'))
-
-
-def save_to_tsv(cik, doc_list, doc_name_list):
-    default_data_path = os.path.join(os.getcwd(), 'SEC-Edgar-Data')
-    # Turn txt docs into tsv files
-    for i in range(len(doc_list)):
-        path = os.path.join(default_data_path, cik, doc_name_list[i])
-        name = os.path.splitext(path)[0]+'.tsv'
-
-        with open(name, 'wt') as outfile:
-            tsv_writer = csv.writer(outfile, delimiter='\t')
-        """
-        with open(path, 'rb') as tsvin, open(name, 'wb') as csvout:
-            tsvin = csv.reader(tsvin, delimiter='\t')
-            csvout = csv.writer(csvout)
-
-            for row in tsvin:
-                count = int(row[4])
-                if count > 0:
-                    csvout.writerows(repeat(row[2:4], count))
-        """
-
-def xml_to_pd():
-    # Modify function later, this is a test one
-    # Load XML 
-    e = et.parse('primary_doc.xml').getroot()
-    # Convert to string
-    xmlstr = et.tostring(e, encoding='ascii', method='xml')
-    # Convert xml str to dict and dump into json
-    print(json.dumps(xmltodict.parse(xmlstr), indent=4))
-    
-
-
-
+# Testing
 if __name__ == "__main__":
-    #parse_doc(ticker='AAPL', cik='0000320193', count=20)
-    #parse_doc(ticker='AMD', cik='0000002488', count=20)
-    parse_doc(ticker='BillBoi', cik='0001166559', prior_to='20021217')
-    #make_dir('00023232', prior_to='20180509')
+    print('Hello World')
