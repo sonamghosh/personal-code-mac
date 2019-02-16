de89b74ae05840169186d798a3cb006c380ff67e
Sonam Ghosh <sonamghosh@gmail.com>
2019-02-16 03:51:24 -0500

    skeleton of class

--- queues/q/solution.py
+++ queues/q/solution.py
@@ -23,6 +23,36 @@ s2 = s2.encode('utf-8')
 s2 = base64.encodebytes(s2)
 s2h = hashlib.sha256(s2).hexdigest()  # hash
 print(s2h)
+# Transformation rules ignore values of private fields ones with _
 
 
 # Dispatch Rules
+# _special -> queue 0
+# _hash -> 1
+# value with muidaQ -> 2
+# int val -> 3
+# Else 4
+# Ignore private field values with underscore _
+
+class MessageService:
+    def __init__(self):
+        continue
+
+    def encode_str(val):
+        utf8_base64_encoded = base64.encodebytes(val.encode('utf-8'))
+        hashed_str = hashlib.sha256(utf8_base64_encoded).hexdigest()
+
+    def transform(msg):
+        for field, val in msg.items():
+            if not field.startswith('_'):
+                if isinstance(val, str) and 'Qadium' in val:
+                    msg[field] = val[::-1]
+                elif isinstance(val, int):
+                    msg[field] = ~val
+            if '_hash' in field:
+                msg['hash'] = encode_str(val)
+
+        return msg
+
+    def dispatch(msg):
+        continue
