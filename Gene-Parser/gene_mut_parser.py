8ab0ac187d9126f64dd3a51294b83f969f6e587d
Sonam Ghosh <sonamghosh@gmail.com>
2019-03-19 16:50:03 -0400

    Playing around with the example queries

--- Gene-Parser/gene_mut_parser.py
+++ Gene-Parser/gene_mut_parser.py
@@ -1,3 +1,25 @@
 import requests
 import numpy as np
 import pandas as pd
+import json
+
+# Mutation Query
+url1 = 'http://www.cbioportal.org/webservice.do?'+\
+        'cmd=getProfileData&genetic_profile_id=gbm_tcga_mutations'+\
+        '&id_type=gene_symbol&gene_list=TP53&case_set_id=gbm_tcga_cnaseq'
+
+# Copy Number allterations for TP53
+url2 = 'http://www.cbioportal.org/webservice.do?'+\
+    'cmd=getProfileData&genetic_profile_id=gbm_tcga_gistic'+\
+    '&id_type=gene_symbol&gene_list=TP53&case_set_id=gbm_tcga_cnaseq'
+
+
+# Test Requests to see how output looks like
+
+#req1 = requests.get(url1)
+#print(req1.text)
+
+#req2 = requests.get(url2)
+#print(req2.text)
+#pretty_json = json.loads(req2.text)
+#print(json.dumps(pretty_json, indent=2))
