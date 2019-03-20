5ff106c25b64001dbe577cd0c752e63bfc9c0f11
Sonam Ghosh <sonamghosh@gmail.com>
2019-03-19 22:25:04 -0400

    Running initial experiments on counting Nans

--- Gene-Parser/gbm_parser.py
+++ Gene-Parser/gbm_parser.py
@@ -2,6 +2,7 @@ import requests
 import numpy as np
 import pandas as pd
 import json
+import io
 
 # Mutation Query
 url1 = 'http://www.cbioportal.org/webservice.do?'+\
@@ -16,9 +17,39 @@ url2 = 'http://www.cbioportal.org/webservice.do?'+\
 
 # Test Requests to see how output looks like
 
-#req1 = requests.get(url1)
-#print(req1.text)
+req1 = requests.get(url1)
+print(req1.text)
+print(req1.encoding)
 
+
+d = req1.text
+split_txt1 = d.split("TP53", 1)[1]
+print(split_txt1)
+print(len(split_txt1))
+d_list = split_txt1.split("\t")
+d_list.pop(0)
+print(d_list)
+if d_list[-1] == 'NaN\n':
+    print('yes')
+else:
+    print('no')
+
+for i in range(len(d_list)):
+    if d_list[i].endswith("\n"):
+        print(i)
+        d_list[i] = d_list[i][:-1]
+
+print(len(d_list))
+print(d_list)
+nan_nums = d_list.count('NaN')
+print(nan_nums)
+print(nan_nums/len(d_list) * 100)
+
+#d = d.decode('ISO-8859-1').encode('utf8')
+
+#print(req1.content)
+#rawData = pd.read_csv(req1.text, encoding='ISO-8859-1', dtype=object)
+#rint(rawData)
 #req2 = requests.get(url2)
 #print(req2.text)
 #pretty_json = json.loads(req2.text)
