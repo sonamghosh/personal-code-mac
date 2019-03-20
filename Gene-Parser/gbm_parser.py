25a355f6f73061ab7c9b2fc0a82e1b183f855e7d
Sonam Ghosh <sonamghosh@gmail.com>
2019-03-19 23:18:03 -0400

    created first function for parsing data

--- Gene-Parser/gbm_parser.py
+++ Gene-Parser/gbm_parser.py
@@ -43,7 +43,40 @@ print(len(d_list))
 print(d_list)
 nan_nums = d_list.count('NaN')
 print(nan_nums)
-print(nan_nums/len(d_list) * 100)
+print(100 - nan_nums/len(d_list) * 100)
+
+
+def parse_mutation_data(gene):
+    """
+    insert documentation here
+    Poggers
+
+    note:
+    consider adding more params for genetic_profile_id
+    and case_set_id
+    """
+    # Link to query
+    url = 'http://www.cbioportal.org/webservice.do?'+\
+          'cmd=getProfileData&genetic_profile_id=gbm_tcga_mutations'+\
+          '&id_type=gene_symbol&gene_list='+gene+'&case_set_id=gbm_tcga_cnaseq'
+    # Send Request to Database
+    req = requests.get(url)
+
+    data = req.text
+    # Get all information after the string that shows the Gene
+    data = data.split(gene.upper(), 1)[1]
+    # Turn string into a list by looking for tab deliminator
+    data_list = data.split("\t")
+    # Get Rid of first value which is a blank
+    data_list = data_list.pop(0)  # TODO: Consider removing any potential blanks(?)
+    # Get rid of \n in elements
+    for i in range(len(data_list)):
+        if data_list[i].endswith("\n"):
+            # Remove \n and update entry
+            data_list[i] = data_list[i][:-1]  # TODO: Check if this only happens on the last elem and avoid loop
+
+    return data_list
+
 
 #d = d.decode('ISO-8859-1').encode('utf8')
 
